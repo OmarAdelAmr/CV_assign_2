63032e37307997d7b5c6244ead9fcfbc905387ac
Omar Adel <omaradel9545@gmail.com>
2016-11-25 04:26:02 +0200

    [ADD - ORGANIZE] plate recog. almost done

--- main.py
+++ main.py
@@ -96,39 +96,6 @@ def question_1_connected_component_labeling_l3():
 
 # question_1_connected_component_labeling_l1()
 # question_1_connected_component_labeling_l3()
-
-def try1():
-    img = cv.imread("L3_gray_front.jpg")
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-    # Now we split the image to 5000 cells, each 20x20 size
-    cells = [np.hsplit(row, 100) for row in np.vsplit(gray, 50)]
-
-    # Make it into a Numpy array. It size will be (50,100,20,20)
-    x = np.array(cells)
-
-    # Now we prepare train_data and test_data.
-    train = x[:, :50].reshape(-1, 400).astype(np.float32)  # Size = (2500,400)
-    test = x[:, 50:100].reshape(-1, 400).astype(np.float32)  # Size = (2500,400)
-
-    # Create labels for train and test data
-    k = np.arange(10)
-    train_labels = np.repeat(k, 250)[:, np.newaxis]
-    test_labels = train_labels.copy()
-
-    # Initiate kNN, train the data, then test it with test data for k=1
-    knn = cv.KNearest()
-    knn.train(train, train_labels)
-    ret, result, neighbours, dist = knn.find_nearest(test, k=5)
-
-    # Now we check the accuracy of classification
-    # For that, compare the result with test_labels and check which are wrong
-    matches = result == test_labels
-    correct = np.count_nonzero(matches)
-    accuracy = correct * 100.0 / result.size
-    print accuracy
-
-
 # L = [(1, 2, 3, 8), (4,), (1, 3), (2, 3), (4, 5, 8), (5, 6,), (6, 7,)]
 # s = [set(i) for i in L if i]
 # print find_intersection(s)
---
+++ test.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+
+#######   training part    ###############
+samples = np.loadtxt('trained_data/generalsamples2.data', np.float32)
+responses = np.loadtxt('trained_data/generalresponses2.data', np.float32)
+responses = responses.reshape((responses.size, 1))
+
+model = cv2.KNearest()
+model.train(samples, responses)
+
+
+############################# testing part  #########################
+
+
+def sort_contours(cnts, method="left-to-right"):
+    # initialize the reverse flag and sort index
+    reverse = False
+    i = 0
+
+    # handle if we need to sort in reverse
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+
+    # handle if we are sorting against the y-coordinate rather than
+    # the x-coordinate of the bounding box
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+
+    # construct the list of bounding boxes and sort them from top to
+    # bottom
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes), key=lambda b: b[1][i], reverse=reverse))
+
+    # return the list of sorted contours and bounding boxes
+    return (cnts, boundingBoxes)
+
+
+im = cv2.imread('plates/L1_front.jpg')
+plate_height, plate_width = im.shape[:2]
+x_min = plate_width / 19
+x_max = plate_width / 2.3
+y_min = plate_height / 2.4
+y_max = plate_height / 1.15
+part = im[y_min:y_max, x_min:x_max]
+part = cv2.resize(part, (int(x_max - x_min) / 3, int(y_max - y_min) / 3))
+
+out = np.zeros(part.shape, np.uint8)
+gray = cv2.cvtColor(part, cv2.COLOR_BGR2GRAY)
+
+thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
+
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+print type(contours)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)
+(contours, boundingBoxes) = sort_contours(contours)
+print type(contours)
+
+string_cumulative = ""
+
+for cnt in contours:
+    if cv2.contourArea(cnt) > 50:  # was 50
+        print cv2.contourArea(cnt)
+        [x, y, w, h] = cv2.boundingRect(cnt)
+        if h > 28:
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            roi = thresh[y:y + h, x:x + w]
+            roismall = cv2.resize(roi, (10, 10))
+            roismall = roismall.reshape((1, 100))
+            roismall = np.float32(roismall)
+            retval, results, neigh_resp, dists = model.find_nearest(roismall, k=1)
+            string = str(int((results[0][0])))
+            string_cumulative += string
+            cv2.putText(out, string, (x, y + h), 0, 1, (0, 255, 0))
+
+print string_cumulative
+cv2.imshow('im', im)
+cv2.imshow('out', out)
+cv2.imshow('part', part)
+cv2.imshow("thresh", thresh)
+cv2.waitKey(0)
