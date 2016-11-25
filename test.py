ec3c3e7fe3a7db9b67345a1b4ccfc8c298c02af5
Omar Adel <omaradel9545@gmail.com>
2016-11-25 05:36:34 +0200

    [ADD] no big changes

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ import cv2
 import numpy as np
 
 #######   training part    ###############
-samples = np.loadtxt('trained_data/generalsamples2.data', np.float32)
-responses = np.loadtxt('trained_data/generalresponses2.data', np.float32)
+# samples = np.loadtxt('trained_data/generalsamples.data', np.float32)
+# responses = np.loadtxt('trained_data/generalresponses.data', np.float32)
+
+samples = np.loadtxt('generalsamples.data', np.float32)
+responses = np.loadtxt('generalresponses.data', np.float32)
+
 responses = responses.reshape((responses.size, 1))
 
 model = cv2.KNearest()
@@ -37,18 +41,26 @@ def sort_contours(cnts, method="left-to-right"):
 
 
 im = cv2.imread('plates/L1_front.jpg')
+# im = cv2.imread('plates/L2_front.png')
+# im = cv2.imread('plates/L3_front.png')
+# im = cv2.imread('plates/L4_front.png')
+
 plate_height, plate_width = im.shape[:2]
 x_min = plate_width / 19
 x_max = plate_width / 2.3
 y_min = plate_height / 2.4
-y_max = plate_height / 1.15
+y_max = plate_height / 1.1
 part = im[y_min:y_max, x_min:x_max]
-part = cv2.resize(part, (int(x_max - x_min) / 3, int(y_max - y_min) / 3))
+
+div_factor = 3
+if plate_width <= 400:
+    div_factor = 1
+
+part = cv2.resize(part, (int(x_max - x_min) / div_factor, int(y_max - y_min) / div_factor))
 
 out = np.zeros(part.shape, np.uint8)
 gray = cv2.cvtColor(part, cv2.COLOR_BGR2GRAY)
-
-thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
+thresh = cv2.adaptiveThreshold(gray, 50, 1, 1, 11, 2)
 
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 print type(contours)
@@ -60,11 +72,11 @@ string_cumulative = ""
 
 for cnt in contours:
     if cv2.contourArea(cnt) > 50:  # was 50
-        print cv2.contourArea(cnt)
         [x, y, w, h] = cv2.boundingRect(cnt)
         if h > 28:
             cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
             roi = thresh[y:y + h, x:x + w]
+            cv2.imshow("roi", roi)
             roismall = cv2.resize(roi, (10, 10))
             roismall = roismall.reshape((1, 100))
             roismall = np.float32(roismall)
@@ -76,6 +88,7 @@ for cnt in contours:
 print string_cumulative
 cv2.imshow('im', im)
 cv2.imshow('out', out)
-cv2.imshow('part', part)
-cv2.imshow("thresh", thresh)
+
+cv2.imshow('thresh', thresh)
+cv2.imshow('gray', gray)
 cv2.waitKey(0)
